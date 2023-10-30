07619f943dcb5ad84b1ffaa33849d6519d73c754
Devin <guozwhero@126.com>
2023-10-30 21:08:46 +0800

    webassembliy
    webassembliy

---
+++ www/index.js
@@ -0,0 +1,25 @@
+
+
+async function init() {
+    const importObject = {
+        console: {
+            log: () => {
+                console.log("Just logging something!");
+            },
+            error: () => {
+                console.log("I am just error.");
+            }
+        }
+    }
+
+    const response = await fetch("sum.wasm");
+    const buffer = await response.arrayBuffer();
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
+
+    const sumFunction = wasm.instance.exports.sum;
+    debugger
+    const result = sumFunction(500, 300);
+    console.log(result);
+}
+
+init();
